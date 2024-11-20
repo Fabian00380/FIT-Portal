0f686249408d27593178f7137122443de6365975
Naimul Islam Fabian <md.naimulislam100@gmail.com>
2024-11-21 04:08:23 +0600

    Header changes

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FIT Portal</title>
+    <link rel="stylesheet" href="styles/basic.css">
+    <link rel="stylesheet" href="styles/landing-page.css">
+</head>
+<body></body>
